999bf6e99d3d4dda4fd224f47e6f6dafa82b60e0
LadyKerr <kadiekerr@gmail.com>
2019-04-24 20:49:20 -0400

    started lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+
+//Parent
+class Person {
+    constructor(attrs) {
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+        this.gender = attrs.gender;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}. `;
+    };
+};
+
+//Child
+class Instructor extends Person {
+    constructor(instructAttrs) {
+        super(instructAttrs);
+        this.specialty = instructAttrs.specialty;
+        this.favLanguage = instructAttrs.favLanguage;
+        this.catchPhrase = instructAttrs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(studentObj, subject) {
+        console.log(`${student.name} receives a perfect score of ${subject}.`);
+    };
+};
