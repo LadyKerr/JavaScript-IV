2068db290f7268c341dda176839d9f852638b63c
LadyKerr <kadiekerr@gmail.com>
2019-04-25 20:30:55 -0400

    object classes completed. MVP done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,5 @@
 // CODE here for your Lambda Classes
 
-//Parent
 class Person {
     constructor(attrs) {
         this.name = attrs.name;
@@ -14,25 +13,20 @@ class Person {
 };
 
 //Person Objects
-const Taylor = new Person ({
+const taylor = new Person ({
     name: 'Taylor',
     age: 25,
     location: 'Australia',
     gender: 'Female'
 });
-console.log(Taylor);
-console.log(Taylor.speak());
 
-const Alexis = new Person({
+const alexis = new Person({
     name: 'Alexis',
     age: 30,
     location: 'California',
     gender: 'Female'
 });
-console.log(Alexis);
-console.log(Alexis.speak());
 
-//extend from Person
 class Instructors extends Person {
     constructor(instructAttrs) {
         super(instructAttrs);
@@ -44,11 +38,31 @@ class Instructors extends Person {
         console.log(`Today we are learning about ${subject}.`);
     }
     grade(student, subject) {
-        console.log(`${student.name} receives a perfect score of ${subject}.`);
+        console.log(`${student} receives a perfect score on ${subject}.`);
     };
 };
 
-//Extending from Person
+//Instructor Objects
+const shavel = new Instructors ({
+    name: 'Shavel',
+    age: 32,
+    location: 'Florida',
+    gender: 'Female',
+    speacialty: 'Ruby',
+    favLanguage: 'Ruby',
+    catchPhrase: 'never stop learning'
+});
+
+const ann = new Instructors({
+    name: 'Ann',
+    age: 42,
+    location: 'Jamaica',
+    gender: 'Non-Binary',
+    speacialty: 'JavaScript!',
+    favLanguage: 'Python',
+    catchPhrase: 'it\'s just JavaScript...'
+});
+
 class Student extends Person {
     constructor(studAttrs) {
         super(studAttrs);
@@ -56,18 +70,38 @@ class Student extends Person {
         this.className = studAttrs.className;
         this.favSubjects = studAttrs.favSubjects;
     }
-    listsSubject(subject) {
-        console.log(favSubjects);
+    listsSubjects() {
+        console.log(`${this.favSubjects}`);
     }
     PRAssignment(subject) {
         console.log(`${this.name} has submitted a PR for ${subject}.`);
     }
     sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on {subject}.`);
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
     };
 };
 
-//Extending from Instructor
+//Student Objects
+const kira = new Student ({
+    name: 'Kira',
+    age: 19,
+    location: 'Orlando, FL',
+    gender: 'female',
+    previousBackground: 'Social Work',
+    className: 'Web16',
+    favSubjects: ['HTML', 'CSS', 'Phython']
+});
+
+const olivea = new Student ({
+    name: 'Olivea',
+    age: 20,
+    location: 'Tampa, FL',
+    gender: 'non-binary',
+    previousBackground: 'Customer Service',
+    className: 'Web16',
+    favSubjects: ['C++', 'PHP', 'Swift']
+});
+
 class ProjectManagers extends Instructors {
     constructor(pMAttrs) {
         super(pMAttrs);
@@ -78,8 +112,59 @@ class ProjectManagers extends Instructors {
         console.log(`${this.name} announces to ${channel}, @channel standy times!`);
     }
     debugsCode(student, subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+        console.log(`${this.name} debugs ${student}'s code on ${subject}`);
     };
 };
+
+//PM Objects
+const mon = new ProjectManagers ({
+    name: 'Monique',
+    age: 25,
+    location: 'California',
+    gender: 'unknown',
+    speacialty: 'Front-End Dev',
+    favLanguage: 'Go',
+    catchPhrase: 'wi jammin!'
+});
+
+const flo = new ProjectManagers ({
+    name: 'Florence',
+    age: 45,
+    location: 'Guyana',
+    gender: 'Female',
+    specialty: 'Full-stack Dev',
+    favLanguage: 'Java',
+    catchPhrase: 'you got this!'
+});
+
+
+console.log(taylor);
+console.log(alexis);
+console.log(taylor.speak());
+console.log(alexis.speak());
+
+console.log(shavel);
+console.log(shavel.speak());
+console.log(shavel.demo('javascript'));
+console.log(shavel.grade('Kira', 'Python'));
+console.log(ann);
+console.log(ann.speak());
+console.log(ann.catchPhrase);
+console.log(ann.grade('Olivea', 'JavaScript'));
+
+console.log(kira);
+console.log(kira.listsSubjects());
+console.log(kira.PRAssignment('Python'));
+console.log(olivea);
+console.log(olivea.listsSubjects());
+console.log(olivea.sprintChallenge('JavaScript'));
+console.log(`${olivea.name}'s previous background was in ${olivea.previousBackground}.`);
+
+console.log(`${mon.name} is enjoying ${mon.location}`);
+console.log(`${flo.name} loves doing her specialty ${flo.specialty}.`);
+console.log(mon.standUp('Web16'));
+console.log(flo.standUp('Web16'));
+console.log(mon.debugsCode('Kira', 'Python'));
+console.log(flo.debugsCode('Olivea', 'JavaScript'));
 
 
